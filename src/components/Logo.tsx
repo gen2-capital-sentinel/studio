405b56ae6eee29064a9ce91e9d0b3b7a92eab309
Oliver Hibbs-Brockway <oliver.brockway@gen2wealth.uk>
2025-10-18 21:01:02 +0000

    can you reduce the logos size in the header banner by 10%

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -6,8 +6,8 @@ export function Logo() {
       <Image
         src="https://pub-11134a6be96f479ebe08254c1e1fa2f6.r2.dev/Gen2_logo.png"
         alt="Gen2 Wealth logo"
-        width={130}
-        height={28}
+        width={117}
+        height={25}
         priority
       />
     </div>
